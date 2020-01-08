2f5af5280b4667c3f2010fd6c69c8591a069a9dc
wanghq-c <wanghq-c@glodon.com>
2020-01-08 13:32:57 +0800

    edit config

---
+++ app-test/src/main/java/com/whq/apptest/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.whq.apptest.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Data
+public class AppConfig {
+    private String name;
+    private String server;
+}
--- app-test/src/main/java/com/whq/apptest/controller/TestController.java
+++ app-test/src/main/java/com/whq/apptest/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.whq.apptest.controller;
 
+import com.whq.apptest.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
+    @Autowired
+    AppConfig appConfig;
     @GetMapping("hi")
     public String hi(String name){
         return "hi"+name;
@@ -15,5 +19,9 @@ public class TestController {
     public String sleep() throws InterruptedException {
         Thread.sleep(5000l);
         return "hi";
+    }
+    @GetMapping("getConfig")
+    public String getConfig(){
+        return appConfig.getName();
     }
 }
