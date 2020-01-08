c4e6391bdcd512deca6bf1993cd60515dc1d1026
wanghq-c <wanghq-c@glodon.com>
2020-01-08 15:15:55 +0800

    add config refresh

--- app-test/src/main/java/com/whq/apptest/controller/TestController.java
+++ app-test/src/main/java/com/whq/apptest/controller/TestController.java
@@ -2,12 +2,14 @@ package com.whq.apptest.controller;
 
 import com.whq.apptest.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
+@RefreshScope
 public class TestController {
     @Autowired
     AppConfig appConfig;
